21bb2f6aadd88f3e79390b72473f9fd27dcfa384
L0la <torok.gabi@gmail.com>
2021-02-01 11:53:41 +0100

    nav menu modify

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,6 +5,7 @@ import "./nav.css";
 const Nav = () => {
     return (
         <div className="mobil-container">
+            <img src={logo} alt="brand felirat"/>
             <li className="nav-item menu__item footer-item">About</li>
             <li className="nav-item menu__item footer-item">Careers</li>
             <li className="nav-item menu__item footer-item">Events</li>
